9a8ac6ca2975ef354a5bc17394afb8f5cc6753e0
Nillouise <LovelyNihilism@outlook.com>
2017-05-22 07:02:45 +0800

    interval

---
+++ Algorithm/喵哈哈村的种树大赛.cpp
@@ -0,0 +1,104 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+
+int n, m, M;
+
+int gcd(int a, int b)
+{
+	if (b == 0)return abs(a);
+	return abs(gcd(b, a%b));
+}
+
+int p, v;
+int gL, gR;
+struct Interval
+{
+	int Tree[1048576 << 2], tree[1048576];
+
+	Interval()
+	{
+		memset(Tree, 0, sizeof(Tree));
+		memset(tree, 0, sizeof(tree));
+	}
+
+	void update(int o, int L, int R)
+	{
+		if (L == R) {
+			Tree[o] += v;
+		}
+		else {
+			int M = L + (R - L) / 2;
+
+			if (p <= M)update(o * 2, L, M);
+			else update(o * 2 + 1, M + 1, R);
+
+			Tree[o] = gcd(Tree[o * 2], Tree[o * 2 + 1]);
+		}
+	}
+
+	int query(int o, int L, int R)
+	{
+		if (gL <= L&&gR >= R)return Tree[o];
+		int M = L + (R - L) / 2;
+		int res = 0;
+		if (gL <= M)res = gcd(res, query(o * 2, L, M));
+		if (gR>M)res = gcd(res, query(o * 2 + 1, M + 1, R));
+		return res;
+	}
+
+	void treeadd(int pos, int addv)
+	{
+		while (pos <= n)
+		{
+			tree[pos] += addv;
+			pos += pos&(-pos);
+		}
+	}
+
+	int treesum(int pos)
+	{
+		int sum = 0;
+		while (pos>0)
+		{
+			sum += tree[pos];
+			pos -= pos&(-pos);
+		}
+		return sum;
+	}
+};
+
+Interval interval;
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	scanf("%d%d", &n, &m);
+	for (int i = 0; i<m; i++)
+	{
+		int ques;
+		scanf("%d", &ques);
+		if (ques == 1)
+		{
+			int addv;
+			scanf("%d%d%d", &gL, &gR, &addv);
+			v = addv;
+			p = gL;
+			interval.update(1, 1, n);
+			p = gR + 1;
+			v = -addv;
+			interval.update(1, 1, n);
+			interval.treeadd(gL, addv);
+			interval.treeadd(gR + 1, -addv);
+		}
+		else
+		{
+			scanf("%d%d", &gL, &gR);
+			gL++;
+			int ans = interval.query(1, 1, n);
+			ans = gcd(ans, interval.treesum(gL - 1));
+			printf("%d\n", ans);
+		}
+	}
+}
--- test/Source.cpp
+++ test/Source.cpp
@@ -1,151 +1,29 @@
-// LA3938 Ray, Pass me the dishes!
-// Rujia Liu
-#include<cstdio>
-#include<cstring>
-#include<algorithm>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
 using namespace std;
 
-typedef pair<int, int> interval;
-const int maxn = 100000;
-typedef long long LL;
-int gL, gR;
-LL prefix_sum[maxn];
-LL sum(int L, int R)
+
+int n, m, M;
+
+
+struct interval
 {
-	return prefix_sum[R] - prefix_sum[L - 1];
-}
+	int Tree[1048576 << 2], tree[1048576];
 
-LL sum(interval a)
-{
-	return sum(a.first, a.second);
-}
-
-interval better(interval a1, interval a2)
-{
-	if (sum(a1) > sum(a2))return a1;
-	else if (sum(a1) < sum(a2))return a2;
-	else return a1 < a2 ? a1 : a2;
-}
-
-struct Btree
-{
-	interval maxsub[maxn];
-	int prefix[maxn];
-	int suffix[maxn];
-
-	void build(int o, int L, int R)
+	interval()
 	{
-		int lc=o*2, rc=o*2+1;
-		int M = L + (R - L) / 2;
-
-		if (L == R)
-		{
-			maxsub[o] = make_pair(L,R);
-			prefix[o] = L;
-			suffix[o] = L;
-		}
-		else {
-			build(lc, L, M);
-			build(rc, M + 1, R);
-
-			LL v1 = sum(L,prefix[lc]);
-			LL v2 = sum(L, prefix[rc]);
-			if (v1 == v2)
-			{
-				prefix[o] = prefix[lc];
-			}
-			else {
-				prefix[o] = v1 > v2 ? prefix[lc] : prefix[rc];
-			}
-
-			v1 = sum(suffix[lc], R);
-			v2 = sum(suffix[rc], R);
-			if (v1 == v2)
-			{
-				suffix[o] = suffix[lc];
-			}
-			else {
-				suffix[o] = v1 > v2 ? suffix[lc] : suffix[rc];
-			}
-
-			maxsub[o] = better(maxsub[lc], maxsub[rc]);// 完全在左子树或者右子树
-			maxsub[o] = better(maxsub[o], make_pair(suffix[lc],prefix[rc]));// 跨越中线
-
-		}
-
-	}
-
-	int queryPre(int o,int L,int R)
-	{
-		if (prefix[o] <= gR)return prefix[o];
-		int M = L + (R - L) / 2;
-		int lc = o * 2, rc = o * 2 + 1;
-		if (gR <= M)return queryPre(lc, L, M);
-		int maxl = queryPre(lc, L, M);
-		int maxr = queryPre(rc, M + 1, R);
-
-		return better(make_pair(L, maxl), make_pair(L, maxr)).second;
-	}
-
-	int querySuf(int o, int L, int R)
-	{
-		if (suffix[o] >= gL)return suffix[o];
-		int rc = o * 2 + 1, lc = 0 * 2;
-		int M = L + (R - L) / 2;
-		if (gL > M)return querySuf(rc, M + 1, R);
-		int maxl = querySuf(lc, L, M);
-		int maxr = querySuf(rc, M + 1, R);
-
-		return better(make_pair(maxl, R), make_pair(maxr, R)).first;
-	}
-
-	interval query(int o, int L, int R)
-	{
-		if (gL <= L&&gR >= R)
-			return maxsub[o];
-		int lc = o * 2, rc = o * 2 + 1;
-		int M = L + (R - L) / 2;
-		if (gR <= M)return query(lc, L, M);
-		if (gL > M)return query(rc, M + 1, R);
-
-		interval i1 = query(lc, L, M);
-		interval i2 = query(rc, M + 1, R);
-		interval i3;
-		i3.first= querySuf(lc, L, M);
-		i3.second = queryPre(rc, M + 1, R);
-
-		interval r = better(i1, i2);
-		r = better(r, i3);
-		return r;
+		memset(Tree,)
 	}
 
 };
 
-Btree tree;
 
-int main() {
+int main()
+{
 	freopen("input.txt", "r", stdin);
 
-	int n, q;
-	for (int kase = 1;; kase++)
-	{
-		if (scanf("%d%d", &n, &q) != 2)break;
-		prefix_sum[0] = 0;
-		for (int i = 0; i < n; i++)
-		{
-			int c; scanf("%d", &c);
-			prefix_sum[i + 1] = prefix_sum[i] + c;
-		}
-		tree.build(1, 1, n);
-		printf("Case %d:\n", kase);
-		while (q--)
-		{
-			scanf("%d%d", &gL, &gR);
-			interval ans = tree.query(1, 1, n);
-			printf("%d %d\n",ans.first,ans.second);
-		}
 
-	}
 
-	return 0;
+
 }
