3aa94cf5b70fe4da414e1f88d27841f4fea567fb
Nillouise <LovelyNihilism@outlook.com>
2017-10-11 18:24:18 +0800

    链表题目

--- Algorithm/uva 12657 特别难.cpp
+++ Algorithm/uva 12657 特别难.cpp
@@ -1,6 +1,4 @@
 //这个双向链表，一开始还忘记了移动一个结点时，要弥合原来的位置了
-
-
 #include <iostream>
 #include<string>
 #include<cstring>
@@ -15,11 +13,11 @@
 #include <bitset>
 using namespace std;
 typedef long long LL;
-const int MAXN = 100000+5;
-int let[MAXN],rit[MAXN];
+const int MAXN = 100000 + 5;
+int let[MAXN], rit[MAXN];
 int inverse = 0;
 int n, m;
-void link(int x,int y)
+void link(int x, int y)
 {
 	rit[x] = y;
 	let[y] = x;
@@ -33,15 +31,15 @@ int main()
 	ios::sync_with_stdio(false);
 
 	int t = 1;
-	while(cin>>n>>m)
+	while (cin >> n >> m)
 	{
-		for(int i =1;i<=n+1;i++)
+		for (int i = 1; i <= n + 1; i++)
 		{
 			link(i - 1, i);
 		}
 
 		inverse = 0;
-		while(m--)
+		while (m--)
 		{
 			int com, a, b;
 			cin >> com;
@@ -53,25 +51,25 @@ int main()
 			{
 				cin >> a >> b;
 
-//				int i = rit[0];
-//				int pos = 1;
-//				while (i != n + 1)
-//				{
-//									cout << i << " ";
-//					i = rit[i];
-//				}
-//				cout << endl;
+				//				int i = rit[0];
+				//				int pos = 1;
+				//				while (i != n + 1)
+				//				{
+				//									cout << i << " ";
+				//					i = rit[i];
+				//				}
+				//				cout << endl;
 				if (com == 3)
 				{
 					//这是错的
-//					//一开始忘记弥合这个la了//这里交换是不用弥合的
-//					int la = let[a];
-//					int ra = rit[a];
-//					int lb = let[b];
-//					int rb = rit[b];
-//					//先弥合，后解决其他问题
-//					link(la, ra);
-//					link(lb, rb);
+					//					//一开始忘记弥合这个la了//这里交换是不用弥合的
+					//					int la = let[a];
+					//					int ra = rit[a];
+					//					int lb = let[b];
+					//					int rb = rit[b];
+					//					//先弥合，后解决其他问题
+					//					link(la, ra);
+					//					link(lb, rb);
 					//好像要先交换了，才能找点
 					if (rit[b] == a)swap(a, b);
 
@@ -80,15 +78,16 @@ int main()
 					int la = let[a];
 					int ra = rit[a];
 
-//					//这里要特别处理这两点是相邻的情况。
-//
-//					if (rit[b] == a)swap(a, b);
-					if(rit[a]==b)
+					//					//这里要特别处理这两点是相邻的情况。
+					//
+					//					if (rit[b] == a)swap(a, b);
+					if (rit[a] == b)
 					{
 						link(a, rb);
 						link(b, a);
 						link(la, b);
-					}else
+					}
+					else
 					{
 						link(lb, a);
 						link(a, rb);
@@ -97,14 +96,14 @@ int main()
 					}
 
 
-//					int lx = let[a];
-//					int rx = rit[a];
-//					int ly = let[b];
-//					int ry = rit[b];
-//					link(lx, b);
-//					link(b, rx);
-//					link(ly, a);
-//					link(a, ry);
+					//					int lx = let[a];
+					//					int rx = rit[a];
+					//					int ly = let[b];
+					//					int ry = rit[b];
+					//					link(lx, b);
+					//					link(b, rx);
+					//					link(ly, a);
+					//					link(a, ry);
 
 				}
 				else
@@ -128,13 +127,13 @@ int main()
 					link(a, b);
 
 				}
-//				else //这里把com==3的情况也包括进来了，下次一定要写请条件
+				//				else //这里把com==3的情况也包括进来了，下次一定要写请条件
 				else if (com == 2)
 				{
 					//是把a割掉，而不是把b割掉
-//					int lb = let[b];
-//					int rb = rit[b];
-//					link(lb, rb);
+					//					int lb = let[b];
+					//					int rb = rit[b];
+					//					link(lb, rb);
 					int la = let[a];
 					int ra = rit[a];
 					link(la, ra);
@@ -150,28 +149,28 @@ int main()
 			}
 		}
 		LL total = 0;
-		if(!inverse)
+		if (!inverse)
 		{
 			int i = rit[0];
 			int pos = 1;
-			while (i != n+1)
+			while (i != n + 1)
 			{
-				if(pos&1)
-				total += i;
-//				cout << i << " ";
+				if (pos & 1)
+					total += i;
+				//				cout << i << " ";
 				i = rit[i];
 				pos++;
 			}
 		}
-		if(inverse)
+		if (inverse)
 		{
 			int i = let[n + 1];
 			int pos = 1;
-			while (i!=0)
+			while (i != 0)
 			{
-				if(pos&1)
-				total += i;
-//				cout << i<<" ";
+				if (pos & 1)
+					total += i;
+				//				cout << i<<" ";
 				i = let[i];
 				pos++;
 			}
