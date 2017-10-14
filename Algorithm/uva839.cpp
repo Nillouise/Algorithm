057f9fe54de928ac0fbcc877904e890ad9979742
Nillouise <LovelyNihilism@outlook.com>
2017-10-14 16:53:32 +0800

    最后一句不用空行，中间的要用空行

---
+++ Algorithm/uva839.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+int dfs()
+{
+	int Wl, Dl, Wr, Dr;
+	cin >> Wl >> Dl >> Wr >> Dr;
+	if (Wl == 0)
+	{
+		Wl = dfs();
+	}
+	if (Wr == 0)
+	{
+		Wr = dfs();
+	}
+	if (Wl == -1 || Wr == -1)
+	{
+		return -1;
+	}
+	if (Wl*Dl == Wr*Dr)
+	{
+		return Wl + Wr;
+	}
+	else {
+		return -1;
+	}
+
+}
+
+int main()
+{
+	freopen("input.txt", "r", stdin);//
+
+	ios::sync_with_stdio(false);
+
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		if (dfs() == -1)
+		{
+			cout << "NO" << endl;
+		}
+		else {
+			cout << "YES" << endl;
+		}
+		if (T != 1)
+			cout << endl;
+	}
+
+
+
+	return 0;
+}
