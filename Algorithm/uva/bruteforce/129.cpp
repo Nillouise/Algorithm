13144357fd1a7106de1d7fcc59aa9ad921e1f98d
Nillouise <LovelyNihilism@outlook.com>
2017-11-17 22:02:47 +0800

    Create 129.cpp

---
+++ Algorithm/uva/bruteforce/129.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+int cnt=0;
+int n,k;
+int dfs(vector<char>& vec)
+{
+  if(cnt++==k)
+  {
+    for(int i=0;i<vec.size();i++)cout<<vec[i];
+    cout<<endl;
+    return 1;
+  }
+  for(char i='A';i<'A'+n;i++)
+  {
+    vec.push_back(i);
+    int flag=1;
+    for(int j=1;2*j<=vec.size();j++)
+    {
+      int equal=0;
+      for(int k=0;k<j;k++)
+      {
+        int tail = vec.size()-1;
+        if(vec[tail-k]!=vec[tail-k-j])
+        {
+          equal=1;
+          break;
+        }
+      }
+      if(equal==0)
+        {
+          flag=0;
+          break;
+        }
+    }
+    if(flag==0){vec.pop_back();continue;}
+    if(dfs(vec))
+      return 1;
+  }
+  return 0;
+}
+
+
+
+int main()
+{
+//    freopen("I:\\Algorithms\\git\\Algorithm\\Algorithm\\input.txt","r",stdin);//
+
+    ios::sync_with_stdio(false);
+
+    while(cin>>n>>k,n!=0)
+    {
+      cnt=0;
+      std::vector<char> vec;
+      dfs(vec);
+    }
+
+    return 0;
+}
