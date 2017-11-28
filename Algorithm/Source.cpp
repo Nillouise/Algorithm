d98ff258d10fc3391f4160f6859807f960403f25
Nillouise <LovelyNihilism@outlook.com>
2017-11-28 19:56:41 +0800

    长度覆盖数量

--- Algorithm/Source.cpp
+++ Algorithm/Source.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long LL;
-int n,k;
-vector<int> data;
+
+
 
 
 int main()
@@ -12,53 +12,76 @@ int main()
 
     ios::sync_with_stdio(false);
     
-    cin>>n>>k;
-    data.resize(n+1);
-    data[0]=-1e9;
-    for (int i = 1; i <=n; i++) 
+    LL n,m,a,d;
+    cin>>n>>m>>a>>d;
+    // vector<int> data(m);//卧槽这题还要long long，我还漏看了
+    vector<LL> data(m);
+    for (int i = 0; i < m; i++) 
     {
         cin>>data[i];
     }
     sort(data.begin(),data.end());
-    data[0]=1e9;
-    int beg=0,end = data.back()-data[1];
-    while(beg<end)
+    m = unique(data.begin(),data.end())-data.begin();
+    
+    LL e=1,c=0;//employee,client
+    // int mod = d/a+((d%a)?1:0);//这么写是错的
+    // LL mod = (d-1)/a+1; // 这里为什么不行？
+    LL mod = d/a+1;
+    LL cnt =0;
+    while(e<=n&&c<m)
     {
-        int mid = (beg+end)>>1;
-        vector<int> DP(n+1);
-        DP[0]=1;
-        vector<int> g(n+1);//前缀和
-        g[0]=1;
-        int j=1;//双指针法
-        for (int i = 1; i <=n; i++) 
+        
+        // cout<<"e "<<e<<" c "<<c<<" cnt "<<cnt<<endl;//
+        if(e*a<data[c])
         {
-            while(data[j]+mid<data[i])j++;
-            if(i-j+1<k)
-            {
-                g[i]=g[i-1];
-                DP[i]=0;
-                continue;
-            }
-            int x = i-k+1;
-            //这里处理j==1好好想想。
-            if(j==1||(j-2>=0&&x-1>=0&&g[x-1]-g[j-2]>0))
-            {
-                DP[i]=1;
-                g[i]=g[i-1]+1;
-            }else{
-                DP[i]=0;
-                g[i]=g[i-1];
-            }
-        }
-        if(DP[n]==1)
-        {
-            end=mid;
+            LL cpre = data[c]/a;//client前面的employee编号
+            cpre = min(n,cpre);
+            LL gapnum = cpre-e+1;
+            // int curcnt = gapnum/mod + (gapnum%mod)?1:0;//卧槽这表达式优先级错了
+            LL curcnt = gapnum/mod + (gapnum%mod?1:0);
+            // cout<<"mod "<<mod<<" "<<gapnum<< " "<<"curcnt"<<curcnt<<endl;//
+            cnt+=curcnt;
+            LL begin = e+ (gapnum-1)/mod *mod;
+            e = begin+mod;
+            
+            // cnt++;
+            // e+=mod;
+            while(c<m&&begin*a+d>=data[c])c++;
         }else{
-            beg=mid+1;
+            cnt++;
+            LL j = c;
+            // LL r = (d-(a-(data[c]%a)));//这里好蠢呀，只要找data[c]+d 之前的门就可以了//顺带一提这个实现也是错的
+            // LL add = r/a+((r%a)?1:0);//这是错的，长度覆盖端点会产生，端点都在线段上的问题
+            // LL add= (r-1)/a+1;
+            // e+=add;
+            e = (data[c]+d)/a+ 1;
+            while(j<m&&data[j]<=data[c]+d)j++;
+            // c=j+1;//这里并不需要+1，我对上面的while语句有很大误解
+            c=j;
         }
     }
-    cout<<end<<endl;
-
+    
+        // cout<<"e "<<e<<" c "<<c<<" cnt "<<cnt<<endl;//
+        
+    if(e<=n)
+    {
+        LL geshu = n-e+1;//个数
+        // LL curcnt = (n-e)/mod +(n-e)%mod ?1:0;//这是错的，用个数才对
+        // LL curcnt = geshu/mod+ geshu%mod?1:0;//这里的表达式优先级又搞错
+        LL curcnt = geshu/mod+(geshu%mod?1:0);
+        // cout<<"curcnt "<<curcnt<<endl;//
+        cnt+=curcnt;
+    }
+    while(c<m)
+    {
+        cnt++;
+        LL j =c;
+        while(j<m&&data[j]<=data[c]+d)j++;
+        
+        // c=j+1;//这里并不需要+1，我对上面的while语句有很大误解
+        c=j;
+    }
+    cout<<cnt<<endl;
 
     return 0;
 }
