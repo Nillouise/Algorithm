295f37da1cab5864025b1c044fde9cdcf3fcb650
Nillouise <LovelyNihilism@outlook.com>
2018-02-11 21:40:19 +0800

    cf

--- Algorithm/CF/cf 461/C.cpp
+++ Algorithm/CF/cf 461/C.cpp
@@ -8,6 +8,14 @@ int main()
   //  freopen("C:\\Users\\37\\Documents\\project\\atom\\Algorithm\\Algorithm\\input.txt","r",stdin);//
 
     ios::sync_with_stdio(false);
-    
-    
+    LL n,m;
+    cin>>n>>m;
+    for(LL i=2;i<=m;i++){
+      if(n%i!=i-1){
+        cout<<"No"<<endl;
+        return 0;
+      }
+    }
+    cout<<"Yes"<<endl;
+    return 0;
 }
