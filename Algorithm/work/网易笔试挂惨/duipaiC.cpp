18c2f8a036c04f69148d6bcd1e1a4a19989e6357
Nillouise <LovelyNihilism@outlook.com>
2018-03-29 20:16:01 +0800

    生命的失利

---
+++ Algorithm/work/网易笔试挂惨/duipaiC.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+
+
+
+int main()
+{
+    //freopen("I:\\Project\\acm\\input.txt","r",stdin);//
+    
+    ios::sync_with_stdio(false);
+    
+    
+    
+    
+    return 0;
+}
