7e55737dff4595185f5a4cf9105a18730c43801a
Nillouise <LovelyNihilism@outlook.com>
2018-04-11 19:08:58 +0800

    卡特兰数

--- Algorithm/luogu/data p1198.cpp
+++ Algorithm/luogu/data p1198.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 typedef long long LL;
 
-
+LL dat[200000+100];
 
 
 int main()
@@ -10,6 +10,41 @@ int main()
     //freopen("I:\\Project\\acm\\Algorithm\\input.txt","r",stdin);//
     
     ios::sync_with_stdio(false);
+    LL n, D;
+    cin>>n>>D;
+    string com;
+    for(int i=0;i<=n;i++)dat[i] = -1e18;
+    LL pre=0;
+    int k = n;
+    for (int i = 0; i < n; i++) {
+        cin>>com;
+        LL x;
+        cin>>x;
+        if(com=="A")
+        {
+            LL nxt = (pre+x)%D;
+            // cout<<"nxt"<<nxt<<endl;//
+            dat[k] = nxt;
+            for (int j = k; j <= n; j+=(j&(-j))) {
+                dat[j] = max(dat[j],nxt);
+            }
+            k--;
+        }else{
+            LL ans = -1e18;
+            //注意这里，当求的段为空的时候，要特判
+            if(x==0){
+                cout<<0<<endl;
+                pre=0;
+                continue;
+            }
+            for(int j = min(k+x,n);j>0;j-=(j&(-j)) )
+            {
+                ans = max(ans,dat[j]); 
+            }
+            cout<<ans<<endl;
+            pre = ans;
+        }
+    }
     
     
     
---
+++ Algorithm/luogu/math p1641.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+//卡特兰数
+LL n,m;
+LL MOD = 20100403;
+
+
+//禁止使用这种递归
+LL pow2(LL x,LL p)
+{
+    if(p==1)
+        return x%MOD;
+    return pow2(x,p/2)*pow2(x,x/2+x%2)%MOD;
+}
+
+LL pow(LL x,LL p)
+{
+    LL res = 1;
+    while(p)
+    {
+        if(p&1)res = (res*x)%MOD;
+        p>>=1;
+        x=(x*x)%MOD;
+    }
+    return res;
+}
+
+LL inv[1000000];
+LL pre[1000000];
+
+//算组合数
+LL Combine(int n,int m)
+{
+    LL up = pre[n]*inv[n-m]%MOD;//算上面组合数公式上面的结果
+    LL down = up *inv[m]%MOD;//算组合式公式下面的结果。
+    return down;
+}
+
+int main()
+{
+    //freopen("I:\\Project\\acm\\Algorithm\\input.txt","r",stdin);//
+    
+    ios::sync_with_stdio(false);
+    
+    cin>>n>>m;
+    
+    pre[1]=1;
+    for (int i = 2; i <= n+m; i++) {//算1*2*3*4...
+        pre[i]=pre[i-1]*i%MOD;
+    }
+    
+    //算1*2*3*4...MOD下的逆元
+    inv[n+m] = pow(pre[n+m],MOD-2);
+    for(int i=n+m-1;i>0;i--)
+    {
+        inv[i] = inv[i+1]*(i+1)%MOD;
+    }
+    
+    cout<<(Combine(n+m,m)-Combine(n+m,m-1)+MOD)%MOD<<endl;
+    
+    
+    
+    
+    return 0;
+}
