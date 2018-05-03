438ac8b6b1f12da51f4febaccb9e9857fd02ede2
Nillouise <LovelyNihilism@outlook.com>
2018-05-03 11:12:07 +0800

    打印double的debug技巧

--- DevC++/main.cpp
+++ DevC++/main.cpp
@@ -1,79 +1,43 @@
-#include<bits/stdc++.h>
-
+#include<cstdio>
+#include<cstring>
 using namespace std;
+
 typedef long long LL;
 
-int dat[1000000];
-int n;
-vector<int> G[1000000];
-vector<int> G2[1000000];
-int ans1[1000000];
-int ans2[1000000];
+const int MAXN = 1005;
 
-void dfs1(int x,int p,int v)
-{
-	if(p==-1){
-		for(auto a:G[x])
-		{
-			dfs1(a,x,dat[a]);
-		}
-		ans1[x] = dat[x];
-	}else{
-		for(auto a:G[x]){
-			if(a!=p)
-				dfs1(a,x,__gcd(dat[x],v));
-		}
-		ans1[x] = __gcd(dat[x],v);
-	}
-}
-
-void dfs2(int x,int p,map<int,int> rm,int dep)
-{
-	int MAX = 1;
-	for(auto &a:rm){
-		if(dat[x]%a.first==0){
-			a.second++;
-		}
-		if(a.second>=dep-1){
-			MAX = max(MAX,a.first);
-		}
-	}
-	for(auto a:G[x]){
-		if(a!=p)
-			dfs2(a,x,rm,dep+1);
-	}
-	ans2[x] = MAX;
-	
-}
-
-
-
+double dp[MAXN][MAXN];
+int n,m;
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin>>n;
-	for(int i=0;i<n;i++){
-		cin>>dat[i+1];
-	}
-	for(int i=0;i<n-1;i++){
-		int u,v;cin>>u>>v;
-		G[u].push_back(v);
-		G[v].push_back(u);
-	}
-	dfs1(1,-1,0);
-	map<int,int> rm;
-	for(int i=1;i*i<=dat[1];i++){
-		if(dat[1]%i==0){
-			rm[i]++;
-			rm[dat[1]/i]++;
-			if(i*i==dat[1])rm[i]--; 
-		}
-	}
-	dfs2(1,-1,rm,1);
-	for(int i=1;i<=n;i++){
-		cout<<max(ans1[i],ans2[i])<<" ";
-	}
-	cout<<endl;
+	
+	while(~scanf("%d%d",&n,&m))
+	{
+		memset(dp,0,sizeof(dp));
+		for(int i=n;i>=0;i--){
+			for(int j=m;j>=0;j--)
+			{
+				if(i==n&&j==m){
+					continue;
+				}
+//				dp[i][j] = (1.0*i/n*(m-j)/m * dp[i][j+1] + 1.0*(n-i)/n*j/m*dp[i+1][j] 
+//							+ 1.0*(n-i)/n*(m-j)/m*dp[i+1][j+1] +1.0)/(1.0 - 1.0*i/n*j/m);
+//							/(1.0 - 1.0*(i+j)/(n+m)); 这是错的 
 
-	return 0;
+     		   double p1, p2, p3, p4;  int s = m;
+				p1 = 1.0*(n-i)*(s-j) / (n*s);  
+                p2 = 1.0*(n-i)*j     / (n*s);  
+                p3 = 1.0*i*(s-j)     / (n*s);  
+                p4 = 1.0*i*j         / (n*s);  
+				dp[i][j] = (p1*dp[i+1][j+1] + p2*dp[i+1][j]  // 状态转移  
+							+ p3*dp[i][j+1]    + 1)  
+							/ (1-p4);  
+			}
+		}
+		
+		printf("%.4lf\n", dp[0][0]);
+	}
+	
+
+    return 0;
 }
